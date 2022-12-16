a55b6b9e78a8171d1995a8c88386e6c42dfd7642
anas nashat <72121781+anasnashat@users.noreply.github.com>
2022-12-16 05:54:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
-# This example requires the 'message_content' privileged intents
+import os, asyncio, time
+from dotenv import load_dotenv
 
-import os
+import time
 import discord
 from discord.ext import commands
 
+load_dotenv()
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', case_insensitive=True, self_bot=True)
+
+msg = '''
+Crossfire Epic new server for Everyone EG,NA,BR servers , Download it now and get 150k ZP 
+Website: 
+https://cfepic.com/
+Download:
+https://www.mediafire.com/file/oohjr4em6n6irtf/CF_EPIC_
+Discord:
+https://discord.gg/cfepic
+'''
 
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 
-bot.run(os.environ["DISCORD_TOKEN"])
+@bot.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('!epic'):
+        # dm all members of the guild
+        for member in message.guild.members:
+            try:
+                await member.send(msg)
+                print(f"Sent message to {member}")
+                time.sleep(25)
+            except:
+                pass
+
+    else:
+        await bot.process_commands(message)
+
+
+Token = 'MTA0NDA5NTE5MDgwNzM1MTM3Ng.GdTbqe.jO1VgRHUaGA9cFiKb8g9Rm_JqmAbaJg01LtQwU'
+bot.run(Token)
+
+# important run bot from shell not from run to work
